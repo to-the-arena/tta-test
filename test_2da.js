751bdb29b26c5ffc58169e7794c7aef4bdb3ceb4
to-the-arena <tothearenagame@gmail.com>
2019-02-14 20:37:24 +0100

    Added max velocity

--- test_2da.js
+++ test_2da.js
@@ -48,6 +48,7 @@ function sword(x, y, w, h, com_y) {
 	this.r = 0;
 	this.w = w;
 	this.h = h;
+	this.vMax = 50;
 
 	// com = Center of mass
 	this.com = [0.5,com_y];
@@ -61,21 +62,8 @@ function sword(x, y, w, h, com_y) {
 	ctx.fillStyle = "FF0000";
 
 	this.update = function() {
-
 		//Change in Coordiantes of entity
 		sMove(parent);
-
-		//Check for attack
-		if ((mouse.sx - mouse.fx != 0 || mouse.sy - mouse.fy != 0) && mouse.button == 0) {	
-			// dir is angle between mouse movement and Handle vector
-			var dir = ((mouse.sx - mouse.fx) * (Math.sin(parent.r) * parent.h * parent.com[1] + mouse.sx - mouse.fx) + (mouse.sy - mouse.fy) * ( - Math.cos(parent.r) * parent.h * parent.com[1] + mouse.sy - mouse.fy)) / (Math.sqrt(Math.pow(mouse.sx - mouse.fx, 2) + Math.pow(mouse.sy - mouse.fy, 2)) * Math.sqrt(Math.pow(Math.sin(parent.r) * parent.h * parent.com[1] + mouse.sx - mouse.fx, 2) + Math.pow( - Math.cos(parent.r) * parent.h * parent.com[1] + mouse.sy - mouse.fy, 2)));
-		};
-
-		if (dir >= 0.8) {
-			this.attack("slash");
-		} else if (dir <= - 0.8) {
-			this.attack("stab");
-		} else {this.attack(null)};
 		
 		//Positioning and Drawing
 		ctx.save();
@@ -92,28 +80,50 @@ function sword(x, y, w, h, com_y) {
 
 //Sword Move 
 function sMove(parent) {
-	// hx, hy Handle Position nhx, nhy new Handle position
-	hx = Math.sin(parent.r) * parent.h * parent.com[1] + parent.x;
-	hy = - Math.cos(parent.r) * parent.h * parent.com[1] + parent.y;
-	nhx = hx + mouse.sx - mouse.fx;
-	nhy = hy + mouse.sy - mouse.fy;
 
-	//Find new com Position in new x (nx) and new y (ny)
-	nx = nhx - (nhx - mySword.x) * Math.sqrt(Math.pow(hx - mySword.x , 2) + Math.pow(hy - mySword.y , 2)) / Math.sqrt(Math.pow(nhx - mySword.x , 2) + Math.pow(nhy - mySword.y , 2));
-	ny = nhy - (nhy - mySword.y) * Math.sqrt(Math.pow(hx - mySword.x , 2) + Math.pow(hy - mySword.y , 2)) / Math.sqrt(Math.pow(nhx - mySword.x , 2) + Math.pow(nhy - mySword.y , 2));
-	
-	//Find new angle in new r (nr)
-	if (nhx - nx <= 0) {
-		nr = Math.acos((nhy - ny) / Math.sqrt(Math.pow(nhx - nx , 2) + Math.pow(nhy - ny , 2)));
-	} else {
-		nr = - Math.acos((nhy - ny) / Math.sqrt(Math.pow(nhx - nx , 2) + Math.pow(nhy - ny , 2)));
-	};
+	if (mouse.button == 0 && (mouse.sx != mouse.fx || mouse.sy != mouse.fy)) {
+		//check for max velocity; dx, dy change in handle position
+		if (Math.sqrt(Math.pow(mouse.sx - mouse.fx, 2) + Math.pow(mouse.sy - mouse.fy, 2)) > parent.vMax) {
+			dx = (mouse.sx - mouse.fx) / Math.sqrt(Math.pow(mouse.sx - mouse.fx, 2) + Math.pow(mouse.sy - mouse.fy, 2)) * parent.vMax;
+			dy = (mouse.sy - mouse.fy) / Math.sqrt(Math.pow(mouse.sx - mouse.fx, 2) + Math.pow(mouse.sy - mouse.fy, 2)) * parent.vMax;
+		} else {
+			dx = mouse.sx - mouse.fx;
+			dy = mouse.sy - mouse.fy;
+		};
+		
+	 	// hx, hy Handle Position; nhx, nhy new Handle position
+		hx = Math.sin(parent.r) * parent.h * parent.com[1] + parent.x;
+		hy = - Math.cos(parent.r) * parent.h * parent.com[1] + parent.y;
+		nhx = hx + dx;
+		nhy = hy + dy;
 
-	if (mouse.button == 0) {
+		//Find new com Position in new x (nx) and new y (ny)
+		nx = nhx - (nhx - mySword.x) * Math.sqrt(Math.pow(hx - mySword.x , 2) + Math.pow(hy - mySword.y , 2)) / Math.sqrt(Math.pow(nhx - mySword.x , 2) + Math.pow(nhy - mySword.y , 2));
+		ny = nhy - (nhy - mySword.y) * Math.sqrt(Math.pow(hx - mySword.x , 2) + Math.pow(hy - mySword.y , 2)) / Math.sqrt(Math.pow(nhx - mySword.x , 2) + Math.pow(nhy - mySword.y , 2));
+		
+		//Find new angle in new r (nr)
+		if (nhx - nx <= 0) {
+			nr = Math.acos((nhy - ny) / Math.sqrt(Math.pow(nhx - nx , 2) + Math.pow(nhy - ny , 2)));
+		} else {
+			nr = - Math.acos((nhy - ny) / Math.sqrt(Math.pow(nhx - nx , 2) + Math.pow(nhy - ny , 2)));
+		};
+
+		//Check for attack
+		if ((dx != 0 || dy != 0) && mouse.button == 0) {	
+			// dir is angle between mouse movement and Handle vector
+			var dir = ((dx) * (Math.sin(parent.r) * parent.h * parent.com[1] + dx) + (dy) * ( - Math.cos(parent.r) * parent.h * parent.com[1] + dy)) / (Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2)) * Math.sqrt(Math.pow(Math.sin(parent.r) * parent.h * parent.com[1] + dx, 2) + Math.pow( - Math.cos(parent.r) * parent.h * parent.com[1] + dy, 2)));
+		};
+
+		if (dir >= 0.8) {
+			parent.attack("slash");
+		} else if (dir <= - 0.8) {
+			parent.attack("stab");
+		} else {parent.attack(null)};
+
 		//update Positon and Rotation	
 		if (nr < - 0.5 * Math.PI || nr > 0.7 * Math.PI) {
-			parent.x = mySword.x + mouse.sx - mouse.fx;
-			parent.y = mySword.y + mouse.sy - mouse.fy;
+			parent.x += dx;
+			parent.y += dy;
 		} else {
 			parent.x = nx;
 			parent.y = ny;
